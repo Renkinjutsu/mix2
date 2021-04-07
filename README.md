156de4ee0700682fae8a0051e68c423bf1d8b5b7
Andrew Hsieh <andrewhsieh1208@gmail.com>
2021-04-06 23:20:57 -0400

    clean up, fix README, and add requirements.txt

--- README.md
+++ README.md
@@ -6,21 +6,9 @@ easy to use audio editor/compiler
 *This part needs work
 
 1. you need python/pip
-2. I also set this up in venv, I don't think you have to
-
-`pip install numpy`
-
-`pip install audiofile`
-
-`pip install soundfile`
-
-`pip install playsound`
-
-`pip install pyobjc`
-
-`pip install librosa`
-
-*yes this should be in a requirements.txt
+2. create virtualenv
+3. `brew install sox`
+4. `pip install requirements.txt`
 
 # Run
 
--- audiocompiler.py
+++ audiocompiler.py
@@ -8,22 +8,10 @@ import librosa
 
 
 sig, fs = af.read('pianotest.mp3')
-# print(f'sampling rate: {fs}, signal shape: {sig.size}')
-# sound = NSSound.alloc()
-# url = NSURL.URLWithString_('file://pianotest.mp3')
-# sound.initWithContentsOfURL_byReference_(url, True)
-# print("sound", sound.volume, sound.duration)
-# sound.play()
-# playsound('pianotest.mp3')
-# playsound('test2.aiff')
 
 file_path = "pianotest.mp3"
 samples, sampling_rate = librosa.load(file_path, sr =None, mono = True, offset = 0.0, duration = None)
 sample2, sampling_rate2 = librosa.load("test2.aiff", sr =None, mono = True, offset = 0.0, duration = None)
-# y, sr = librosa.load('pianotest.mp3')
-
-# S = np.abs(librosa.stft(samples))
-print("123",len(samples), sampling_rate, "t2", sample2.size, sampling_rate2)
 
 
 if samples.size  > sample2.size:
@@ -31,40 +19,37 @@ if samples.size  > sample2.size:
   newArray = np.repeat(0, appendNum)
   print("newArray", newArray)
   sample2 = np.append(sample2, newArray)
-  # for x in range(appendNum):
-  #   sample2.append(0)
   print("sample2", sample2)
 elif samples.size < sample2.size:
   appendNum = sample2.size - samples.size
   newArray = np.repeat(0, appendNum)
   print("newArray", newArray)
   samples = np.append(samples, newArray)
-  # for x in range(appendNum):
-  #   samples.append(0)
   print("sample", samples)
 
 print("new length", samples.size, sample2.size)
 if samples.size == sample2.size:
   print("go time")
-  tmpArr= []
-  for x in range(round(samples.size)):
-    newValue = (samples[x] + sample2[x]) / 2
-    tmpArr.append(newValue)
 
-  print("tmp", len(tmpArr))
+  tmpArr = [
+    (samples[x] + sample2[x]) / 2
+    for x in range(round(samples.size))
+  ]
+
   newSoundArr = np.array(tmpArr)
 
   print("NEW SOUND ARR", newSoundArr.size)
 
-  print("something")
   sf.write("newSound.wav", newSoundArr, sampling_rate, subtype='PCM_24')
   print("it is written")
 
 print("it is playing")
 playsound("newSound.wav")
 
-print("something2")
 
-# print("s", S) 
-# librosa.display.waveplot(samples)
-# len(samples), sampling_rate
+# 1. find reliable compression and expansion - regulate sampling rate
+# 2. merge 2 clips of different lengths will merge together (tacking on no sound at the end to the clips are equal). maybe loop at end?
+# 3. autotuning - python modules for autotuning. consider only allowing for 1 autotuning setting
+# 4. XXX @Dru take a look at this: https://github.com/jiaaro/pydub/blob/master/API.markdown
+# 5. play as it's compiling instead of compiling to new file? further investigation (punt it to future because chunking mp3 files is a big lift)
+# 6. regulate volume? 
